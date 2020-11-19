1b4fdafc1a59202b7b067d6f91610878a3198d3e
Reka Palanki <reka.palanki@gmail.com>
2020-11-19 12:56:55 +0000

    Completing js counter and adding Clear button.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,12 @@
 let a = 0;
-function myFunction() {
+let b = 0;
+function myFunction1() {
   document.getElementById('counter-box1').innerText = a += 1;
 };
+function myFunction2() {
+  document.getElementById('counter-box2').innerText = b += 1;
+};
+function clearFunction1() {
+  document.getElementById('counter-box1').innerText = a = 0;
+  document.getElementById('counter-box2').innerText = b = 0;
+}
